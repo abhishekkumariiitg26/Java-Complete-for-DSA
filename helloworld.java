24c97c15f95877f0d6067db42cbd6937396b3c1f
Abhishek Kumar <abhishek.kumar.iiitg.26@gmail.com>
2025-05-17 22:23:53 +0530

    \n added

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,8 @@
 class helloworld{
     public static void main(String[] args){
         System.out.println("Hello world");
+        System.out.print("Hello world\n");
+        System.out.println("Hello world");
     }
 
   
